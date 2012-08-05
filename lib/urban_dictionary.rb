8c0a7ee54bbf36cb83fbfb8fc8986388e032d0f4
Ryan Greenberg <ryangreenberg@gmail.com>
2012-08-05 15:54:40 -0700

    Add static methods to UrbanDictionary

--- lib/urban_dictionary.rb
+++ lib/urban_dictionary.rb
@@ -1,4 +1,25 @@
+require 'uri'
+require 'net/http'
+
 require 'urban_dictionary/version'
+require 'urban_dictionary/word'
+require 'urban_dictionary/entry'
 
 module UrbanDictionary
+  DEFINE_URL = 'http://www.urbandictionary.com/define.php'
+  RANDOM_URL = 'http://www.urbandictionary.com/random.php'
+
+  def self.define(str)
+    Word.from_url("#{DEFINE_URL}?term=#{URI.encode(str)}")
+  end
+
+  def self.random_word
+    url = URI.parse(RANDOM_URL)
+    req = Net::HTTP::Get.new(url.path)
+    rsp = Net::HTTP.start(url.host, url.port) {|http|
+      http.request(req)
+    }
+
+    Word.from_url(rsp['location'])
+  end
 end
