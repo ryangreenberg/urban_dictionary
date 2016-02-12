06a25b15e134b2c2b7a59c05d6c315521fbfe0ff
Ryan Greenberg <ryangreenberg@gmail.com>
2016-02-12 13:47:03 -0800

    Use do ... end for multiline blocks

--- lib/urban_dictionary.rb
+++ lib/urban_dictionary.rb
@@ -16,9 +16,9 @@ module UrbanDictionary
   def self.random_word
     url = URI.parse(RANDOM_URL)
     req = Net::HTTP::Get.new(url.path)
-    rsp = Net::HTTP.start(url.host, url.port) {|http|
+    rsp = Net::HTTP.start(url.host, url.port) do |http|
       http.request(req)
-    }
+    end
 
     Word.from_url(rsp['location'])
   end
