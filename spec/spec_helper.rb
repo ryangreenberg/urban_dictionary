9f72e53a5846b04d2bacb18ef15a52fb86d096fc
Ryan Greenberg <ryangreenberg@gmail.com>
2016-02-15 10:46:37 -0800

    Add helper for loading HTML fixtures in tests

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,6 +4,11 @@ RSpec.configure do |config|
   config.treat_symbols_as_metadata_keys_with_true_values = true
   config.run_all_when_everything_filtered = true
   config.filter_run :focus
-
   config.order = 'random'
+end
+
+module Test
+  def self.load_fixture(name)
+    File.read(File.expand_path("../html/#{name}", __FILE__))
+  end
 end
