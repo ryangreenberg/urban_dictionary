f56f3da346f1295487d958566021889553dd5edc
Ryan Greenberg <ryangreenberg@gmail.com>
2016-02-15 10:46:54 -0800

    Update tests to use new fixtures

--- spec/urban_dictionary/word_spec.rb
+++ spec/urban_dictionary/word_spec.rb
@@ -1,7 +1,7 @@
 require "spec_helper"
 
 describe UrbanDictionary::Word do
-  
+
   describe "instance method" do
     subject{ UrbanDictionary::Word.new("pie", [mock, mock]) }
 
@@ -13,22 +13,19 @@ describe UrbanDictionary::Word do
   describe "class method" do
     describe "#from_url" do
       let(:url){ "the_url" }
-      let(:html_with_results){ File.open(File.expand_path("../../html/test_with_results.html", __FILE__)) }
-      let(:html_without_results){ File.open(File.expand_path("../../html/test_without_results.html", __FILE__)) }
+      let(:html_with_results){ Test.load_fixture("on_fleek_2016-02-15.html") }
+      let(:html_without_results){ Test.load_fixture("sisyphus_2016-02-15.html") }
 
-      it "should parse a valid word" do
-        UrbanDictionary::Word.should_receive(:open).with(url).and_return(html_with_results)
-
-        word = UrbanDictionary::Word.from_url(url)
+      it "parses a valid word" do
+        word = UrbanDictionary::Word.from_html(html_with_results)
         word.should be_a(UrbanDictionary::Word)
-        word.word.should eq("test1234")
-        word.entries.length.should eq(1)
+        word.word.should eq("on fleek")
+        word.entries.length.should eq(7)
         word.entries.first.should be_an_instance_of(UrbanDictionary::Entry)
       end
 
-      it "should gracefully handle an unknown word by returning nil" do
-        UrbanDictionary::Word.should_receive(:open).with(url).and_return(html_without_results)
-        UrbanDictionary::Word.from_url(url).should be_nil
+      it "returns nil for unknown words" do
+        UrbanDictionary::Word.from_html(html_without_results).should be_nil
       end
     end
   end
--- spec/urban_dictionary_spec.rb
+++ spec/urban_dictionary_spec.rb
@@ -5,12 +5,12 @@ describe UrbanDictionary do
 
   describe "class method" do
     describe "#define" do
-      it "should delegate to Word#from_url" do
+      it "delegates to Word#from_url" do
         UrbanDictionary::Word.should_receive(:from_url).with("#{UrbanDictionary::DEFINE_URL}?term=pie").and_return(result)
         UrbanDictionary.define("pie").should eq(result)
       end
 
-      it "should url encode the request" do
+      it "URL-encodes the request" do
         UrbanDictionary::Word.should_receive(:from_url).with("#{UrbanDictionary::DEFINE_URL}?term=asdf%25asdf").and_return(result)
         UrbanDictionary.define("asdf%asdf").should eq(result)
       end
@@ -18,13 +18,14 @@ describe UrbanDictionary do
 
     describe "#random_word" do
       let(:request){ mock :request }
-      let(:response){ {'location' => 'location'} }
+      let(:url) { 'http://example.com' }
+      let(:response){ {'location' => url } }
       let(:http){ mock :http }
 
       it "should do an HTTP GET to determine the random word" do
         Net::HTTP.should_receive(:start).with("www.urbandictionary.com", 80).and_yield(http).and_return(response)
         http.should_receive(:request).with(an_instance_of(Net::HTTP::Get))
-        UrbanDictionary.should_receive(:define).with('location').and_return(result)
+        UrbanDictionary::Word.should_receive(:from_url).with(url).and_return(result)
         UrbanDictionary.random_word.should eq(result)
       end
     end
