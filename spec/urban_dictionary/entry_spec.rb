8f8736622ea703ee5b0b45afadbfb82fb25912b4
Ryan Greenberg <ryangreenberg@gmail.com>
2016-02-15 11:47:48 -0800

    Convert specs to RSpec 2.14.8 syntax with Transpec
    This conversion was done by Transpec 3.2.1 with the following command:
        transpec
    
      - 9 conversions
          from: obj.should
            to: expect(obj).to
    
      - 5 conversions
          from: mock('something')
            to: double('something')
    
      - 5 conversions
          from: obj.should_receive(:message)
            to: expect(obj).to receive(:message)
    
      - 3 conversions
          from: its(:attr) { }
            to: describe '#attr' do subject { super().attr }; it { } end
    
    For more details: https://github.com/yujinakayama/transpec#supported-conversions

--- spec/urban_dictionary/entry_spec.rb
+++ spec/urban_dictionary/entry_spec.rb
@@ -3,7 +3,7 @@ require "spec_helper"
 describe UrbanDictionary::Entry do
   describe "#to_s" do
     it "should print out the formatted example and definition" do
-      UrbanDictionary::Entry.new("pie", "delicious").to_s.should eq("pie\ndelicious")
+      expect(UrbanDictionary::Entry.new("pie", "delicious").to_s).to eq("pie\ndelicious")
     end
   end
 end
