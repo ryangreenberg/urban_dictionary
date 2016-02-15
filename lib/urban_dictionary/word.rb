684f25e5c9c8d8d8fbb56bbddbda74223c8e5220
Ryan Greenberg <ryangreenberg@gmail.com>
2016-02-15 10:47:23 -0800

    Fix broken detection of words with no definition
    This change also separates .from_url and .from_html which simplifies testing
    by not making the developer stub network methods.

--- lib/urban_dictionary/word.rb
+++ lib/urban_dictionary/word.rb
@@ -1,3 +1,5 @@
+# encoding: UTF-8
+
 require 'nokogiri'
 require 'open-uri'
 
@@ -8,17 +10,33 @@ module UrbanDictionary
     # Can raise SocketError if unable to connect to specified URL
     def self.from_url(url)
       html = open(url).read
-      doc = Nokogiri::HTML(html)
-
-      if doc.css('.word').any?
-        word = doc.css('.word').first.content.strip
-        definitions = doc.css('div.meaning').map{|d| d.content.strip }
-        examples = doc.css('.example').map{|e| e.content.strip }
-        entries = definitions.zip(examples).map{|d,e| Entry.new(d, e)}
-
-        Word.new(word, entries)
-      end
+      from_html(html)
     end
+
+    def self.from_html(html)
+      doc = Nokogiri.HTML(html)
+      words = doc.css('.word')
+      return nil if words.empty?
+
+      word = words.first.content.strip
+      definitions = doc.css('div.meaning').map {|d| d.content.strip }
+      examples = doc.css('.example').map {|e| e.content.strip }
+      entries = definitions.zip(examples).map {|d,e| Entry.new(d, e) }
+
+      defined_word?(word, definitions) ? Word.new(word, entries) : nil
+    end
+
+    # Currently when a word has no definition the result page returns the
+    # shrug emoticon (¯\_(ツ)_/¯) and the text "There aren't any definitions
+    # for [word] yet."
+    def self.defined_word?(word, definitions)
+      undefined_word = (
+        definitions.size == 1 &&
+        definitions[0] =~ /^There aren't any definitions for/
+      )
+      !undefined_word
+    end
+    private_class_method :defined_word?
 
     def initialize(word, entries)
       @word = word
